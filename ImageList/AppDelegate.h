f6464d1ee8a998911cf2d3ae2e8a54625a99adc1
shahas <shahas@bullfin.ch>
2016-05-10 14:22:43 +0530

    Initial Commit

---
+++ ImageList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageList
+//
+//  Created by Shahas on 10/05/16.
+//  Copyright © 2016 Shahas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
